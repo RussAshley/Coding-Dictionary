520375e0f6a54e5f0d0ec588e11648af5152789d
ninjabredman <russellashley@hotmail.co.uk>
2021-10-08 16:48:20 +0100

    Add project files.

---
+++ Coding Dictionary/DefinitionDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coding_Dictionary
+{
+    class DefinitionDatabase
+    {
+        private int ID;
+        private string Term;
+         private string Definition;
+        private string Image;
+        private string URL;
+
+        public DefinitionDatabase(int iD, string term, string definition, string image, string uRL)
+        {
+            ID1 = iD;
+            Term1 = term;
+            Definition1 = definition;
+            Image1 = image;
+            URL1 = uRL;
+        }
+
+        public int ID1 { get => ID; set => ID = value; }
+        public string Term1 { get => Term; set => Term = value; }
+        public string Definition1 { get => Definition; set => Definition = value; }
+        public string Image1 { get => Image; set => Image = value; }
+        public string URL1 { get => URL; set => URL = value; }
+    }
+}
---
+++ Coding Dictionary/LandingPage.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Coding_Dictionary
+{
+    partial class LandingPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1Search = new System.Windows.Forms.Button();
+            this.button1CodingRequirements = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(215, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dictionary of Coding Terms";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1Search
+            // 
+            this.button1Search.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1Search.Location = new System.Drawing.Point(58, 400);
+            this.button1Search.Name = "button1Search";
+            this.button1Search.Size = new System.Drawing.Size(105, 31);
+            this.button1Search.TabIndex = 1;
+            this.button1Search.Text = "Search";
+            this.button1Search.UseVisualStyleBackColor = true;
+            // 
+            // button1CodingRequirements
+            // 
+            this.button1CodingRequirements.BackColor = System.Drawing.Color.Goldenrod;
+            this.button1CodingRequirements.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1CodingRequirements.Location = new System.Drawing.Point(96, 106);
+            this.button1CodingRequirements.Name = "button1CodingRequirements";
+            this.button1CodingRequirements.Size = new System.Drawing.Size(233, 23);
+            this.button1CodingRequirements.TabIndex = 2;
+            this.button1CodingRequirements.Text = "Coding Requirements and Planning";
+            this.button1CodingRequirements.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Goldenrod;
+            this.button2.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(96, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Coding Design";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Goldenrod;
+            this.button3.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(96, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(234, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Implementation";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Goldenrod;
+            this.button4.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(479, 106);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(249, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Testing";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Goldenrod;
+            this.button5.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(479, 194);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(249, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Management";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Goldenrod;
+            this.button6.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(479, 268);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(249, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Misc.";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // LandingPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1CodingRequirements);
+            this.Controls.Add(this.button1Search);
+            this.Controls.Add(this.label1);
+            this.Name = "LandingPage";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1Search;
+        private System.Windows.Forms.Button button1CodingRequirements;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
